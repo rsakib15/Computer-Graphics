744373bf31c2887fb8263091ff401ae9d2d81d31
sakib <srskb15@gmail.com>
2017-10-26 08:55:58 +0600

    Update Readme

--- Batman/main.cpp
+++ Batman/main.cpp
@@ -19,92 +19,6 @@ void DrawGrid(){
     glFlush();
 }
 
-void DrawTrianglePolyGon(){
-    glColor3f(0.0,0.0,0.0);
-    glBegin(GL_POLYGON);
-        glVertex2i(220,60);
-        glVertex2i(380,60);
-        glVertex2i(220,180);
-    glEnd();
-    glFlush();
-}
-
-void DrawTriangleLine(){
-    glColor3f(0.0,0.0,0.0);
-    glBegin(GL_LINES);
-        glVertex2i(20,60);
-        glVertex2i(20,180);
-
-        glVertex2i(20,60);
-        glVertex2i(180,60);
-
-        glVertex2i(180,60);
-        glVertex2i(20,180);
-    glEnd();
-    glFlush();
-}
-
-void DrawTriangleQuad(){
-glColor3f(0.0,0.0,0.0);
-    glBegin(GL_QUADS);
-        glVertex2i(420,60);
-        glVertex2i(420,180);
-
-        glVertex2i(420,180);
-        glVertex2i(580,60);
-    glEnd();
-    glFlush();
-}
-
-
-void DrawRectangleQuad(){
-glColor3f(0.0,0.0,0.0);
-    glBegin(GL_QUADS);
-        glVertex2i(420,240);
-        glVertex2i(420,400);
-
-        glVertex2i(420,400);
-        glVertex2i(580,400);
-
-        glVertex2i(580,400);
-        glVertex2i(580,240);
-
-        glVertex2i(580,240);
-        glVertex2i(420,240);
-    glEnd();
-    glFlush();
-}
-
-void DrawRectanglePolyGon(){
-    glColor3f(0.0,0.0,0.0);
-    glBegin(GL_POLYGON);
-        glVertex2i(220,400);
-        glVertex2i(380,400);
-        glVertex2i(380,240);
-        glVertex2i(220,240);
-    glEnd();
-    glFlush();
-}
-
-void DrawRectangleLine(){
-    glColor3f(0.0,0.0,0.0);
-    glBegin(GL_LINES);
-        glVertex2i(20,240);
-        glVertex2i(20,400);
-
-        glVertex2i(20,400);
-        glVertex2i(180,400);
-
-        glVertex2i(180,400);
-        glVertex2i(180,240);
-
-        glVertex2i(180,240);
-        glVertex2i(20,240);
-    glEnd();
-    glFlush();
-}
-
-
 void myInit(){
     glClearColor(1.0,0.0,0.0,0.0);
     glMatrixMode(GL_PROJECTION);
@@ -115,13 +29,6 @@ void myInit(){
 void CallFunctions(){
     glClear(GL_COLOR_BUFFER_BIT);
     DrawGrid();
-    DrawTriangleLine();
-    DrawTrianglePolyGon();
-    DrawTriangleQuad();
-
-    DrawRectanglePolyGon();
-    DrawRectangleQuad();
-    DrawRectangleLine();
 }
 
 int main(int argv,char** argc){
